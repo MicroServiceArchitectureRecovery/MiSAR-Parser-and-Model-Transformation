b9c1bb1ef62d4898d3b9c461b18b71069f21813c
kevinvahdat01 <117815726+kevinvahdat01@users.noreply.github.com>
2023-10-12 11:45:03 +0100

    Docker segment fully refactored!
    The segment that handles the docker information of the input micro company is now separated into a different python file without any adverse effect.

---
+++ Parser/NewParser/MisarParserDocker.py
@@ -0,0 +1,97 @@
+import yaml
+import re
+
+def yaml_to_dict(filename):
+    yaml_dict = {}
+    with open(filename, encoding='utf8') as file:
+        yaml_dict = yaml.load(file, Loader=yaml.FullLoader)
+    return yaml_dict
+
+def docker_container_part1(docker_compose_files, multi_module_project_name):
+    application_containers = {}
+    for docker_compose_file in docker_compose_files:
+        docker_compose_dict = {}
+        if docker_compose_file.endswith(('.yml','.yaml')):
+            docker_compose_dict = yaml_to_dict(docker_compose_file)
+        if 'services' in docker_compose_dict:
+            docker_compose_dict = docker_compose_dict['services']
+        for container_name in docker_compose_dict:
+            if 'build' in docker_compose_dict[container_name] or 'image' in docker_compose_dict[container_name]:
+                if container_name not in application_containers:
+                    application_containers[container_name] = {}
+                    application_containers[container_name]['parent'] = multi_module_project_name
+                    application_containers[container_name]['filename'] = docker_compose_file
+                    application_containers[container_name]['build'] = ''
+                    application_containers[container_name]['image'] = ''
+                    application_containers[container_name]['logging'] = False
+                    application_containers[container_name]['ports'] = []
+                    application_containers[container_name]['links'] = []
+                if 'image' in docker_compose_dict[container_name]:
+                    if application_containers[container_name]['image'] == '':
+                        application_containers[container_name]['image'] = docker_compose_dict[container_name]['image']
+                if 'build' in docker_compose_dict[container_name]:
+                    if application_containers[container_name]['build'] == '':
+                        if isinstance(docker_compose_dict[container_name]['build'], str):
+                            application_containers[container_name]['build'] = docker_compose_dict[container_name]['build']
+                        elif isinstance(docker_compose_dict[container_name]['build'], dict):
+                            if 'context' in docker_compose_dict[container_name]['build']:
+                                application_containers[container_name]['build'] = docker_compose_dict[container_name]['build']['context']
+                if 'logging' in docker_compose_dict[container_name] or 'log_opt' in docker_compose_dict[container_name]:
+                     if not application_containers[container_name]['logging']:
+                        application_containers[container_name]['logging'] = True
+                if 'ports' in docker_compose_dict[container_name]:
+                    for port in docker_compose_dict[container_name]['ports']:
+                        if port not in application_containers[container_name]['ports']:
+                            application_containers[container_name]['ports'].append(port)
+                if 'expose' in docker_compose_dict[container_name]:
+                    for port in docker_compose_dict[container_name]['expose']:
+                        if port not in application_containers[container_name]['ports']:
+                            application_containers[container_name]['ports'].append(port)
+                if 'links' in docker_compose_dict[container_name]:
+                    for link in docker_compose_dict[container_name]['links']:
+                        if link not in application_containers[container_name]['links']:
+                            application_containers[container_name]['links'].append(link)
+                if 'depends_on' in docker_compose_dict[container_name]:
+                    for link in docker_compose_dict[container_name]['depends_on']:
+                        if link not in application_containers[container_name]['links']:
+                            application_containers[container_name]['links'].append(link)
+    return application_containers
+
+def docker_container_part2(application_containers, app_root_dir):
+    for container_name in application_containers:
+        # ASSUMPTION: every container_name container that has a local project must have a 'build' value
+        # that matches the root directory of the project
+        dockerfile_build_dir = re.findall(r'[\.*\./]*(.+)', application_containers[container_name]['build'])
+        if len(dockerfile_build_dir) > 0:
+            dockerfile_build_dir = dockerfile_build_dir[0].rstrip('/')
+        if dockerfile_build_dir:
+            dockerfile_files = fetch_artifacts('Dockerfile', dockerfile_build_dir, app_root_dir)
+            if len(dockerfile_files) > 0:
+                with open(dockerfile_files[0], encoding='utf8') as dockerfile_file:
+                    for line in dockerfile_file:
+                        line = line.strip()
+                        from_commands = re.findall(r'FROM\s+(.+)', line)
+                        expose_commands = re.findall(r'EXPOSE\s+(.+)', line)
+                        if len(from_commands) > 0:
+                            application_containers[container_name]['image'] = from_commands[0]
+                        elif len(expose_commands) > 0:
+                            application_containers[container_name]['ports'].append(expose_commands[0])
+
+def docker_container_part3(application_containers, application, metamodel):
+    for container_name in application_containers:
+        container = metamodel.DockerContainerDefinition()
+        container.ContainerName = container_name
+        container.BuildField = application_containers[container_name]['build']
+        container.ImageField = application_containers[container_name]['image']
+        container.GeneratesLogs = application_containers[container_name]['logging']
+        container.ParentProjectName = application_containers[container_name]['parent']
+        container.ArtifactFileName = application_containers[container_name]['filename']
+        for port in application_containers[container_name]['ports']:
+            ports = metamodel.DockerContainerPort(ExposesPortsField = port, ParentProjectName = application_containers[container_name]['parent'], ArtifactFileName = application_containers[container_name]['filename'])
+            container.ports.append(ports)
+        order = 0
+        for link in application_containers[container_name]['links']:
+            order = order + 1
+            links = metamodel.DockerContainerLink(DependencyOrder = order, LinksDependsOnField = link, ParentProjectName = application_containers[container_name]['parent'], ArtifactFileName = application_containers[container_name]['filename'])
+            container.links.append(links)
+        application.containers.append(container)
--- Parser/NewParser/MisarParserMain.py
+++ Parser/NewParser/MisarParserMain.py
@@ -7,13 +7,10 @@
 # 05/10/2023
 ############################
 
-import tkinter
-from tkinter import filedialog
-from tkinter import messagebox
+from MisarParserDocker import *
 from pyecore.resources import ResourceSet, URI
 from pyecore.utils import DynamicEPackage
 import os
-import yaml
 import xmltodict
 from collections import OrderedDict
 import re
@@ -30,12 +27,6 @@ def fetch_artifacts(filename_part, filepath_part, app_root_dir):
                 if filepath_part in root:
                     artifact_list.append(artifact_filename)
     return artifact_list
-
-def yaml_to_dict(filename):
-    yaml_dict = {}
-    with open(filename, encoding='utf8') as file:
-        yaml_dict = yaml.load(file, Loader=yaml.FullLoader)
-    return yaml_dict
 
 def xml_to_dict(filename):
     xml_dict = {}
@@ -349,94 +340,48 @@ def evaluate_method_field(field1, method1, type_name, properties, module_name, a
     return field_value
 
 
-def docker_container_part1(docker_compose_files, multi_module_project_name):
-    application_containers = {}
-    for docker_compose_file in docker_compose_files:
-        docker_compose_dict = {}
-        if docker_compose_file.endswith(('.yml','.yaml')):
-            docker_compose_dict = yaml_to_dict(docker_compose_file)
-        if 'services' in docker_compose_dict:
-            docker_compose_dict = docker_compose_dict['services']
-        for container_name in docker_compose_dict:
-            if 'build' in docker_compose_dict[container_name] or 'image' in docker_compose_dict[container_name]:
-                if container_name not in application_containers:
-                    application_containers[container_name] = {}
-                    application_containers[container_name]['parent'] = multi_module_project_name
-                    application_containers[container_name]['filename'] = docker_compose_file
-                    application_containers[container_name]['build'] = ''
-                    application_containers[container_name]['image'] = ''
-                    application_containers[container_name]['logging'] = False
-                    application_containers[container_name]['ports'] = []
-                    application_containers[container_name]['links'] = []
-                if 'image' in docker_compose_dict[container_name]:
-                    if application_containers[container_name]['image'] == '':
-                        application_containers[container_name]['image'] = docker_compose_dict[container_name]['image']
-                if 'build' in docker_compose_dict[container_name]:
-                    if application_containers[container_name]['build'] == '':
-                        if isinstance(docker_compose_dict[container_name]['build'], str):
-                            application_containers[container_name]['build'] = docker_compose_dict[container_name]['build']
-                        elif isinstance(docker_compose_dict[container_name]['build'], dict):
-                            if 'context' in docker_compose_dict[container_name]['build']:
-                                application_containers[container_name]['build'] = docker_compose_dict[container_name]['build']['context']
-                if 'logging' in docker_compose_dict[container_name] or 'log_opt' in docker_compose_dict[container_name]:
-                     if not application_containers[container_name]['logging']:
-                        application_containers[container_name]['logging'] = True
-                if 'ports' in docker_compose_dict[container_name]:
-                    for port in docker_compose_dict[container_name]['ports']:
-                        if port not in application_containers[container_name]['ports']:
-                            application_containers[container_name]['ports'].append(port)
-                if 'expose' in docker_compose_dict[container_name]:
-                    for port in docker_compose_dict[container_name]['expose']:
-                        if port not in application_containers[container_name]['ports']:
-                            application_containers[container_name]['ports'].append(port)
-                if 'links' in docker_compose_dict[container_name]:
-                    for link in docker_compose_dict[container_name]['links']:
-                        if link not in application_containers[container_name]['links']:
-                            application_containers[container_name]['links'].append(link)
-                if 'depends_on' in docker_compose_dict[container_name]:
-                    for link in docker_compose_dict[container_name]['depends_on']:
-                        if link not in application_containers[container_name]['links']:
-                            application_containers[container_name]['links'].append(link)
-    return application_containers
+def multi_module_part1(multi_module_project_name, app_build_files):
+    multi_module_project = {}
+    multi_module_project['parent'] = multi_module_project_name
+    multi_module_project['build'] = ''
+    for app_build_file in app_build_files:
+        multi_module_project['build'] += app_build_file + ';'
+    multi_module_project['build'] = multi_module_project['build'].rstrip(';')
+    multi_module_project_artifact_Id = multi_module_project_name
+    if len(app_build_files) == 1:
+        pom_xml = xml_to_dict(app_build_files[0])
+        if 'project' in pom_xml:
+            if 'artifactId' in pom_xml['project']:
+                multi_module_project_artifact_Id = pom_xml['project']['artifactId']
+    multi_module_project['artifactId'] = multi_module_project_artifact_Id
+    multi_module_project['modules'] = {}
+    return multi_module_project
 
-def docker_container_part2(application_containers, app_root_dir):
-    for container_name in application_containers:
-        # ASSUMPTION: every container_name container that has a local project must have a 'build' value
-        # that matches the root directory of the project
-        dockerfile_build_dir = re.findall(r'[\.*\./]*(.+)', application_containers[container_name]['build'])
-        if len(dockerfile_build_dir) > 0:
-            dockerfile_build_dir = dockerfile_build_dir[0].rstrip('/')
-        if dockerfile_build_dir:
-            dockerfile_files = fetch_artifacts('Dockerfile', dockerfile_build_dir, app_root_dir)
-            if len(dockerfile_files) > 0:
-                with open(dockerfile_files[0], encoding='utf8') as dockerfile_file:
-                    for line in dockerfile_file:
-                        line = line.strip()
-                        from_commands = re.findall(r'FROM\s+(.+)', line)
-                        expose_commands = re.findall(r'EXPOSE\s+(.+)', line)
-                        if len(from_commands) > 0:
-                            application_containers[container_name]['image'] = from_commands[0]
-                        elif len(expose_commands) > 0:
-                            application_containers[container_name]['ports'].append(expose_commands[0])
-
-def docker_container_part3(application_containers, application, metamodel):
-    for container_name in application_containers:
-        container = metamodel.DockerContainerDefinition()
-        container.ContainerName = container_name
-        container.BuildField = application_containers[container_name]['build']
-        container.ImageField = application_containers[container_name]['image']
-        container.GeneratesLogs = application_containers[container_name]['logging']
-        container.ParentProjectName = application_containers[container_name]['parent']
-        container.ArtifactFileName = application_containers[container_name]['filename']
-        for port in application_containers[container_name]['ports']:
-            ports = metamodel.DockerContainerPort(ExposesPortsField = port, ParentProjectName = application_containers[container_name]['parent'], ArtifactFileName = application_containers[container_name]['filename'])
-            container.ports.append(ports)
-        order = 0
-        for link in application_containers[container_name]['links']:
-            order = order + 1
-            links = metamodel.DockerContainerLink(DependencyOrder = order, LinksDependsOnField = link, ParentProjectName = application_containers[container_name]['parent'], ArtifactFileName = application_containers[container_name]['filename'])
-            container.links.append(links)
-        application.containers.append(container)
+def multi_module_part2(module_build_dirs, module_build_files, multi_module_project, multi_module_project_name, application_containers):
+    # create modules for application project
+    for module_build_dir in module_build_dirs:
+        # get module name from project folder name
+        module_name = os.path.basename(module_build_dir)
+        # get module name from container name
+        for container_name, container in application_containers.items():
+            if 'build' in container:
+                if module_name in container['build']:
+                    module_name = container_name
+            if 'image' in container:
+                if module_name in container['image']:
+                    module_name = container_name
+        # get module name from POM build file
+        build_file = ''
+        for module_build_file in module_build_files:
+            if module_build_file.startswith(module_build_dir):
+                build_file = module_build_file
+        multi_module_project['modules'][module_name] = {}
+        multi_module_project['modules'][module_name]['parent'] = multi_module_project_name
+        multi_module_project['modules'][module_name]['build'] = build_file
+        multi_module_project['modules'][module_name]['artifactId'] = module_name
+        multi_module_project['modules'][module_name]['libraries'] = []
+        multi_module_project['modules'][module_name]['properties'] = []
+        multi_module_project['modules'][module_name]['java_elements'] = []
 
 def parser(txt_proj_name, txt_proj_dir, txt_psm_ecore, lst_docker_compose, lst_app_build, lst_module_build_dir, lst_module_build, lst_app_config_dir):
     start_time = datetime.now().strftime("%H:%M:%S")
@@ -494,20 +439,7 @@ def parser(txt_proj_name, txt_proj_dir, txt_psm_ecore, lst_docker_compose, lst_a
     docker_container_part3(application_containers, application, metamodel)
 
     # parse multi module project build artifacts (pom.xml / build.gradle) into application project and its module projects
-    multi_module_project = {}
-    multi_module_project['parent'] = multi_module_project_name
-    multi_module_project['build'] = ''
-    for app_build_file in app_build_files:
-        multi_module_project['build'] += app_build_file + ';'
-    multi_module_project['build'] = multi_module_project['build'].rstrip(';')
-    multi_module_project_artifact_Id = multi_module_project_name
-    if len(app_build_files) == 1:
-        pom_xml = xml_to_dict(app_build_files[0])
-        if 'project' in pom_xml:
-            if 'artifactId' in pom_xml['project']:
-                multi_module_project_artifact_Id = pom_xml['project']['artifactId']
-    multi_module_project['artifactId'] = multi_module_project_artifact_Id
-    multi_module_project['modules'] = {}
+    multi_module_project = multi_module_part1(multi_module_project_name, app_build_files)
 
     # create application project instance
     application_project = metamodel.ApplicationProject()
@@ -515,30 +447,9 @@ def parser(txt_proj_name, txt_proj_dir, txt_psm_ecore, lst_docker_compose, lst_a
     application_project.ArtifactFileName = multi_module_project['build']
     application_project.ProjectArtifactId = multi_module_project['artifactId']
 
+
     # create modules for application project
-    for module_build_dir in module_build_dirs:
-        # get module name from project folder name
-        module_name = os.path.basename(module_build_dir)
-        # get module name from container name
-        for container_name, container in application_containers.items():
-            if 'build' in container:
-                if module_name in container['build']:
-                    module_name = container_name
-            if 'image' in container:
-                if module_name in container['image']:
-                    module_name = container_name
-        # get module name from POM build file
-        build_file = ''
-        for module_build_file in module_build_files:
-            if module_build_file.startswith(module_build_dir):
-                build_file = module_build_file
-        multi_module_project['modules'][module_name] = {}
-        multi_module_project['modules'][module_name]['parent'] = multi_module_project_name
-        multi_module_project['modules'][module_name]['build'] = build_file
-        multi_module_project['modules'][module_name]['artifactId'] = module_name
-        multi_module_project['modules'][module_name]['libraries'] = []
-        multi_module_project['modules'][module_name]['properties'] = []
-        multi_module_project['modules'][module_name]['java_elements'] = []
+    multi_module_part2(module_build_dirs, module_build_files, multi_module_project, multi_module_project_name, application_containers)
 
     # create libraries and properties instances for every module project
     for module_name in multi_module_project['modules']:
